907d99efb6c27dd229fd400fc283a306ff2a5af8
Sajjad Ahmed <sajjad@thesoftaims.com>
2022-12-23 19:32:07 +0500

    Fridays Commit

--- src/components/GroupExpenses/GroupExpense.js
+++ src/components/GroupExpenses/GroupExpense.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { useGetGroupsQuery, useGetGroupuserQuery } from '../../services/api/groupApi'
 import Sidebar from '../Sidebar'
@@ -15,14 +15,27 @@ export const GroupExpense = () => {
 	const [group_id, setGroup_id] = useState()
 	const [category, setCategory] = useState('')
 	const [payers_attributes, setPayers_attributes] = useState([])
-
+	const [split, setSplit] = useState();
+	const [userData, setUserData] = useState([])
 
 	const { data } = useGetGroupsQuery({ refetchOnMountOrArgChange: true })
 
-	const { groupUsers, loading, error } = useGroupUsers(group_id);
+	const { groupUsers } = useGroupUsers(group_id);
+
+	// setting user data state when we get group_users
+	useEffect(() => {
+
+		if (groupUsers && groupUsers.length > 0) {
+			setUserData(groupUsers?.map(user => {
+				// user => hash
+				// adding selected, paid and payable to each user of groupUsers
+				// and adding that in userData
+				return { ...user, selected: false, paid: 0, payable: 0 }
+			}))
+		}
+	}, [groupUsers])
 
 	const [addgroupExpenses] = useAddgroupExpensesMutation()
-
 
 	const categoryOptions = [
 		{ value: 'food', label: 'Food' },
@@ -32,13 +45,11 @@ export const GroupExpense = () => {
 		{ value: 'entertainement', label: 'Entertainement' }
 	]
 
-	const SPLIT_OPTIONS = {
-		EQUALLY: "equally",
-		PERCENTAGE: "percentage",
-		UNEQUALLY: "unequally"
-	};
-
-	const [split, setSplit] = useState(SPLIT_OPTIONS.EQUALLY);
+	const splitOptions = [
+		{ value: 'equally', label: 'equally' },
+		{ value: 'percentage', label: 'percentage' },
+		{ value: 'unequally', label: 'unequally' }
+	]
 
 	const content = data?.map(group => {
 		return {
@@ -57,8 +68,9 @@ export const GroupExpense = () => {
 	})
 
 	// Event handler for when a radio button is clicked
-	const handleChangeRadio = (event) => {
-		setSplit(event.target.value);
+	const handleChangeSplit = (value) => {
+		const split = value.value
+		setSplit(split);
 	};
 
 	const handleGroup = (value) => {
@@ -71,33 +83,42 @@ export const GroupExpense = () => {
 		setCategory(category)
 	}
 
+	// updating amount payable of selected users on changing of amount
+	// or selected user count
+	useEffect(() => {
+		let amountPayable = amount / (payers_attributes.length)
+		if (split === 'equally') {
+			setPayers_attributes(payers_attributes.map((user) => { return { ...user, payable: amountPayable } }))
+			setUserData(userData.map(u => u.selected ? { ...u, payable: amountPayable } : { ...u }))
+		}
+	}, [amount, payers_attributes.length])
+
 	const handleSelectedUser = (e) => {
-		const { id, checked, value } = e.target;
+
+		const { id, checked } = e.target;
+
 		if (checked) {
+			setUserData(userData.map(u => u.id == id ? { ...u, selected: true, paid: 0, payable: 0 } : { ...u }))
 			setPayers_attributes([...payers_attributes, { 'user_id': id, 'paid': 0, 'payable': 0 }]);
-		} 
+		}
 		else {
+			setUserData(userData.map(u => u.id == id ? { ...u, selected: false, paid: 0, payable: 0 } : { ...u }))
 			setPayers_attributes(payers_attributes.filter(user => user['user_id'] !== id));
 		}
 	}
+
+
 	const handlePaid = (e) => {
 		const { id, value } = e.target;
-		setPayers_attributes(payers_attributes.map((user) => {
-			if (user.user_id == id) {
-				user['paid'] = value;
-			}
-			return user;
-		}))
-	}
+		setUserData(userData.map((user) => user.id == id ? { ...user, paid: value } : { ...user }))
+		setPayers_attributes(payers_attributes.map((user) => user.user_id == id ? { ...user, paid: value } : { ...user }))
 
+	}
 	const handlePayable = (e) => {
 		const { id, value } = e.target;
-		setPayers_attributes(payers_attributes.map((user) => {
-			if (user.user_id == id) {
-				user['payable'] = value;
-			}
-			return user;
-		}))
+
+		setPayers_attributes(payers_attributes.map((user) => user.user_id == id ? { ...user, payable: value } : { ...user }))
+		setUserData(userData.map((user) => user.id == id ? { ...user, payable: value } : { ...user }))
 	}
 
 	const handleGroupExpense = async (e) => {
@@ -141,32 +162,59 @@ export const GroupExpense = () => {
 					</div>
 					<div>
 						<label>Split By</label>
-						<div className='flex justify-around '>
-							{Object.values(SPLIT_OPTIONS).map((option) => (
-								<label key={option}>
-									<input
-										type="radio"
-										value={option}
-										checked={split === option}
-										onChange={handleChangeRadio}
-									/>
-									{option}
-								</label>
-							))}
+						<div>
+							<Select options={splitOptions} onChange={handleChangeSplit} placeholder='Select Split' className='mt-2 w-25' />
+							<label>Users in this group</label> <br />
+
+							{split === 'equally' &&
+								<>
+									<div className='flex text-2xl justify-between'>
+										<h3>Users</h3>
+										<h3>Amount Paid</h3>
+										<h3>Amount payable</h3>
+									</div>
+
+									<div className='space-y-2' >
+										{userData?.map((user) =>
+											<div key={user.id} className='flex items-center justify-between border p-2'  >
+												<Checkbox className='w-[155px]' id={user.id} onChange={handleSelectedUser} checked={user.selected}>
+													{user.email}
+												</Checkbox>
+												{<InputField id={user.id} type='number' placeholder='Amount Paid' onChange={handlePaid} value={user.paid} />}
+												{
+													<InputField id={user.id} type='number' placeholder='Amount Payable' value={user.payable} onChange={handlePayable} readOnly={split === 'equally'} />}
+											</div>
+										)}
+									</div>
+								</>
+							}
+							{split === 'percentage' &&
+								<>
+									<div className='flex text-2xl justify-between'>
+										<h3>Users</h3>
+										<h3>Amount Paid</h3>
+										<h3>Amount Percentage</h3>
+										<h3>Amount payable</h3>
+									</div>
+									<div className='space-y-2' >
+										{groupUsers?.map(user =>
+											<div key={user.id} className='flex items-center justify-between border p-2'  >
+												<Checkbox className='w-[155px]' id={user.id} onChange={handleSelectedUser} >
+													{user.email}
+												</Checkbox>
+												{<InputField id={user.id} type='number' placeholder='Amount Paid' onChange={handlePaid} />}
+												{<InputField id={user.id} step='0.01'
+													type='number' placeholder='percentage' onChange={handlePaid} />}
+												{<InputField id={user.id} type='number' placeholder='Amount Payable' onChange={handlePayable} />}
+											</div>
+										)}
+									</div>
+								</>}
+
+
 						</div>
 					</div>
-					<div className='space-y-2' >
-						<label>Users in this group</label> <br />
-						{groupUsers?.map(user =>
-							<div key={user.id} className='flex items-center justify-between border p-2'  >
-								<Checkbox className='w-25' id={user.id} onChange={handleSelectedUser} >
-									{user.email}
-								</Checkbox>
-								{<InputField id={user.id} type='number' placeholder='Amount Paid' onChange={handlePaid} />}
-								{<InputField id={user.id} type='number' placeholder='Amount Payable' onChange={handlePayable} />}
-							</div>
-						)}
-					</div>
+
 					<button type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3">Create a Group Expense</button>
 				</form>
 			</div>
--- src/components/GroupExpenses/InputField.js
+++ src/components/GroupExpenses/InputField.js
@@ -5,7 +5,8 @@ const InputField = (props) => {
   return (
     <div>
       <input id={props.id} type={props.type} placeholder={props.placeholder} className='bg-dark bg-white border p-2 rounded-2'
-        onChange={props.onChange} />
+        onChange={props.onChange} readOnly={props.readOnly} value={props.value} defaultValue={props.defaultValue}
+        />
     </div>
   )
 }
